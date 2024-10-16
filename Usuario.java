672dde7b6b75ff9ee8b9f05614eb2a68ab5347aa
Rafael Haro <rafaharotello2003@gmail.com>
2024-10-16 13:07:43 +0200

    Clase usuario actualizada

--- Usuario.java
+++ Usuario.java
@@ -1,2 +1,33 @@
 public class Usuario {
+    private int ID;
+    private String nombre;
+
+    public Usuario(int ID, String nombre) {
+        this.ID = ID;
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "ID=" + ID +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
